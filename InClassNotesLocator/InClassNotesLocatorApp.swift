a97ad375edf8941aceda0cc52939a3ff1d618958
MsJoyA <132179282+MsJoyA@users.noreply.github.com>
2023-08-02 20:40:14 -0400

    Initial Commit

---
+++ InClassNotesLocator/InClassNotesLocatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  InClassNotesLocatorApp.swift
+//  InClassNotesLocator
+//
+//  Created by Ashley on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct InClassNotesLocatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
